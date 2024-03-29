c91e56e2acdf72bbd29115c7573930d62edfb969
Peachjean2004s <sorakhom.tuisan@gmail.com>
2024-03-29 21:02:02 +0700

    add finite state

--- Main.java
+++ Main.java
@@ -1,12 +1,59 @@
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Graph g = new Graph();
-        g.Creategraph();
-        g.prim();
-        g.kruskal();
-        g.shortestPath();
-        
+        int Select = 0;
+        boolean graphCreated = false;
+
+        while (true) {
+            switch (Select) {
+                case 0:
+                    if (!graphCreated) {
+                        System.out.println("---------- Please create a graph ---------- ");
+                        g.Creategraph();
+                        graphCreated = true;
+                    }
+                    System.out.println("---------- MENU ----------");
+                    System.out.println("1. Prim's Algorithm");
+                    System.out.println("2. Kruskal's Algorithm");
+                    System.out.println("3. Shortest Path");
+                    System.out.println("4. CLOSE PROGRAM");
+                    System.out.print("Enter your choice: ");
+                    Select = sc.nextInt();
+                    break;
+                case 1:
+                    g.prim();
+                    System.out.println("-------------------------");
+                    System.out.println("0. Back to the Menus");
+                    System.out.println("4. Close Program");
+                    Select = sc.nextInt();
+                    break;
+                case 2:
+                    g.kruskal();
+                    System.out.println("-------------------------");
+                    System.out.println("0. Back to the Menus");
+                    System.out.println("4. Close Program");
+                    Select = sc.nextInt();
+                    break;
+                case 3:
+                    g.shortestPath();
+                    System.out.println("-------------------------");
+                    System.out.println("0. Back to the Menus");
+                    System.out.println("4. Close Program");
+                    Select = sc.nextInt();
+                    break;
+                case 4:
+                    System.out.println("CLOSE PROGRAM");
+                    return;
+                default:
+                    System.out.println("PLEASE CHOOSE A NUMBER FROM THE MENU");
+                    Select = 0;
+                    break;
+            }
+        }
 
     }
 
