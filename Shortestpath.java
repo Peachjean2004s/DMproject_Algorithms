7878e774b10bc46e980130a5b078368f9e8fe987
Peachjean2004s <sorakhom.tuisan@gmail.com>
2024-03-29 13:32:33 +0700

    new

--- Shortestpath.java
+++ Shortestpath.java
@@ -4,36 +4,54 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Shortestpath {
-    private int vertices;
-    private int[][] matrix ;
-    Scanner sc = new Scanner(System.in);
+    public static void shortestpath(int[][] MatrixGraph, int vertices) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("From vertex: ");
+        int from = sc.nextInt();
+        System.out.print("To vertex: ");
+        int to = sc.nextInt();
 
-    Shortestpath(int v,int [][] m){
-        vertices = v ;
-        this.matrix = m;
-    }
+        if (from < 1 || from > vertices || to < 1 || to > vertices) {
+            System.out.println("Invalid");
+            return;
+        }
+        int[] distance = new int[vertices + 1];
+        int[] parent = new int[vertices + 1]; 
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[from] = 0; 
 
-     public void dijkstra(int form, int to) {
-        int[] dist = new int[vertices];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[form] = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        pq.offer(new int[]{form, 0});
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(vertex -> distance[vertex]));
+        pq.offer(from); 
 
         while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int u = cur[0];
+            int currentVertex = pq.poll();
 
-            for (int v = 0; v < vertices; v++) {
-                if (matrix[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + matrix[u][v] < dist[v]) {
-                    dist[v] = dist[u] + matrix[u][v];
-                    pq.offer(new int[]{v, dist[v]});
+            for (int neighbor = 1; neighbor <= vertices; neighbor++) {
+                if (MatrixGraph[currentVertex][neighbor] != 0) {
+                    int newDistance = distance[currentVertex] + MatrixGraph[currentVertex][neighbor];
+                    if (newDistance < distance[neighbor]) {
+                        distance[neighbor] = newDistance;
+                        parent[neighbor] = currentVertex;
+                        pq.offer(neighbor); 
+                    }
                 }
             }
         }
 
-        System.out.println("Shortest distance from vertex " + form + " to vertex " + to + ": " + dist[to]);
-    }
+        if (distance[to] == Integer.MAX_VALUE) {
+            System.out.println("There is no path from vertex " + from + " to vertex " + to + ".");
+        } else {
+              System.out.println("Edge    Length");
+              int currentVertex = to;
+              StringBuilder pathBuilder = new StringBuilder();
+              while (currentVertex != from) {
+                  int parentVertex = parent[currentVertex];
+                  pathBuilder.insert(0, "" + parentVertex + " - " + currentVertex + "     " + MatrixGraph[currentVertex][parentVertex] + "\n");
+                  currentVertex = parentVertex;
+              }
+              System.out.print(pathBuilder);
+            System.out.println("Shortest distance from vertex " + from + " to vertex " + to + " is : " + distance[to]);
 
+        }
+    }
 }
